3b1ca4d6bf05082bd6b954358fd9e966aecb153a
Charlie Mordant <cmordant1@gmail.com>
2023-05-21 21:05:29 +0200

    industrilaize

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
-Ansible SecureHost
+Ansible Reverse proxy
 =========
 
-* Galaxy: [![Ansible Galaxy](https://img.shields.io/badge/galaxy-tcharl.freeipa_server-660198.svg?style=flat)](https://galaxy.ansible.com/tcharl/freeipa_server)
-* Lint, Tests & requirements: ![Molecule](https://github.com/OsgiliathEnterprise/freeipa_server/workflows/Molecule/badge.svg)
+* Galaxy: [![Ansible Galaxy](https://img.shields.io/badge/galaxy-tcharl.reverse_proxy-660198.svg?style=flat)](https://galaxy.ansible.com/tcharl/reverse_proxy)
+* Lint & requirements: ![Molecule](https://github.com/OsgiliathEnterprise/reverse_proxy/workflows/Molecule/badge.svg)
+* Tests: [![Build Status](https://app.travis-ci.com/OsgiliathEnterprise/reverse_proxy.svg?branch=master)](https://travis-ci.com/OsgiliathEnterprise/reverse_proxy)
 * Chat: [![Join the chat at https://gitter.im/OsgiliathEnterprise/platform](https://badges.gitter.im/OsgiliathEnterprise/platform.svg)](https://gitter.im/OsgiliathEnterprise/platform?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
-Wrapper on top of Ansible freeipa server to configure it in a simple way
+Installs Docker and configures a reverse proxy
+
+
+```
+firewalld_zones:
+  - name: public # optional
+    nics: # optional, will take all the network interfaces of the machine by default
+      - eth0 # optional
+    virtual_hosts:
+      - name: idm.osgiliath.net # Virual host
+        upstream: "idm.internal.osgiliath.net" # upstream server to proxy
+        referer_suffix: "/ipa/ui" # referer header suffix
+        proto: https # upstream proto
+        container: False # proxy container or real VM/bare metal
+        gen_certs: True # use letsencrypt to generate frontend certificate
+        volumes:
+          - "/etc/ipa:/etc/ipa:ro" # additional volumes to mount in proxy
+        additional_nginx_headers:
+          - "proxy_ssl_trusted_certificate /etc/ipa/ca.crt" # Additional nginx headers
+        ports_binding:
+          - "80:80" # mandatory for letsencrypt
+          - "443:443"
+      - name: idm.osgiliath.test
+        ports_binding:
+          - "80:80" # mandatory for letsencrypt
+          - "443:443"
+        gen_certs: yes # lets encrypt will take care of certificates
+```
 
 Requirements
 ------------
@@ -20,7 +48,7 @@ Take a look at the [molecule tests](./molecule/default/converge.yml) tests and t
 Dependencies
 ------------
 
-[ansible.freeipa collection](https://github.com/freeipa/ansible-freeipa)
+[Ansible Container (docker)](https://github.com/OsgiliathEnterprise/ansible-containerization)
 
 License
 -------
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,11 +1,85 @@
 """Role testing files using testinfra."""
 
 
-def test_user_exists(host):
-    command = r"""set -o pipefail && echo '123ADMin'| \
-    kinit admin > /dev/null && \
-    ipa user-find admin | \
-    grep -c 'First name: Admin'"""
-    cmd = host.run(command)
+def test_docker_is_installed(host):
+    with host.sudo():
+        command = """systemctl status docker.service | \
+        grep -c 'active (running)'"""
+        cmd = host.run(command)
     assert '1' in cmd.stdout
 
+
+def test_nginx_proxy_is_installed(host):
+    with host.sudo():
+        command = """docker ps | \
+        grep -c nginx-proxy"""
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_nginx_proxy_port_is_opened(host):
+    with host.sudo():
+        command = """docker ps | \
+        grep -c '80/tcp'"""
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_socat_container_for_group_ip_is_present(host):
+    with host.sudo():
+        command = """
+            docker container inspect groupip.osgiliath.test | \
+            grep -c 'VIRTUAL_HOST=groupip.osgiliath.test'
+        """
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_socat_container_for_external_vm_is_present(host):
+    with host.sudo():
+        command = """
+            docker container inspect externalvm.osgiliath.net | \
+            grep -c 'VIRTUAL_HOST=externalvm.osgiliath.net'
+        """
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_additional_volume_is_mounted_in_nginx_proxy(host):
+    with host.sudo():
+        command = """
+            docker container inspect nginx-proxy | \
+            grep -c '\"Source\": \"/etc/ipa\"'
+        """
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_nginx_proxy_vhost_files_are_present(host):
+    with host.sudo():
+        command = """
+            ls /usr/share/dockerdata/nginx/vhost.d | \
+            grep -c 'externalvm.osgiliath.net_location'
+        """
+        cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_nginx_proxy_vhost_files_contains_target(host):
+    with host.sudo():
+        command = """
+            cd /usr/share/dockerdata/nginx/vhost.d
+            cat externalvm.osgiliath.net_location \
+            | grep -c 'idm.internal.osgiliath.net'
+            """
+        cmd = host.run(command)
+    assert '3' in cmd.stdout
+
+
+def test_docker_interface_is_configured_in_firewalld(host):
+    command = """
+        sudo firewall-cmd --list-all --zone=public | \
+        grep -c 'docker0'
+    """
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
